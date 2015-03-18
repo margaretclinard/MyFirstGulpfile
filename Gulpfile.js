6af0a884ef622c94b0bd698b1cb24219766ad353
Margaret Clinard <margaret.clinard@gmail.com>
2015-03-18 15:02:06 -0500

    added task for autoprefixer

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,11 @@
 
 var gulp = require('gulp'),
     del  = require('del'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    jade = require('gulp-jade'),
+    autoprefixer = require('autoprefixer-core'),
+    postcss = require('gulp-postcss');
+
 
 gulp.task('clean', function () {
   del(['.tmp', 'public']);
@@ -12,5 +16,15 @@ gulp.task('sass', function () {
   gulp
     .src('app/styles/main.scss')
     .pipe(sass())
-    .pipe(gulp.dest('public/css'))
+    .pipe(postcss([ autoprefixer({ browers: ['last 2 version'] }) ]))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('jade', function () {
+  gulp
+    .src('app/**/*.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('public/'));
 });
